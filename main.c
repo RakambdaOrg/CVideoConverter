95fe1c407b9401b9080db222a1b9484079041752
Thomas Couchoud <zerderr@gmail.com>
2017-07-25 21:15:53 +0200

    Improvments

--- main.c
+++ main.c
@@ -6,30 +6,36 @@
 
 typedef struct
 {
-	const char * filename;
+	char * filename;
 	const char * codec;
 	double fps;
+	double duration;
 } VInfos;
 
-VInfos * getVInfos(char * filename);
+VInfos * getVInfos(char * filename, const char * name);
 
 void printVInfos(VInfos * vInfos);
 
 int shouldProcessFile(char * filename);
 
-char * scat(char * s1, char * s2);
+char * scat(char * s1, const char * s2);
+
+char * convertTime(char * out, int time);
+
+char * asMP4(const char * filename);
 
 int main()
 {
 	av_register_all();
 	avcodec_register_all();
 	
-	char folderInW[] = "***REMOVED***";
-	char folderOutW[] = "***REMOVED***";
+	char folderInWindows[] = "***REMOVED***";
+	char folderOutWindows[] = "***REMOVED***";
 	
-	char dirName[] = "***REMOVED***";
+	//char folderInMac[] = "/Users/mrcraftcod/Dropbox/Tha/Line/";
+	char folderInMac[] = "***REMOVED***";
 	char filePath[512];
-	DIR * dir = opendir(dirName);
+	DIR * dir = opendir(folderInMac);
 	
 	struct dirent * file;
 	while((file = readdir(dir)) != NULL)
@@ -37,34 +43,41 @@ int main()
 		if(!shouldProcessFile(file->d_name))
 			continue;
 		
-		sprintf(filePath, "%s/%s", dirName, file->d_name);
-		VInfos * vInfos = getVInfos(filePath);
-		if(vInfos->fps > 0 && strcmp(vInfos->codec, "h264") == 0)
+		sprintf(filePath, "%s/%s", folderInMac, file->d_name);
+		VInfos * vInfos = getVInfos(filePath, file->d_name);
+		if((vInfos->fps > 0 && vInfos->fps <= 30) && strcmp(vInfos->codec, "h264") == 0)
 		{
-			if(vInfos->fps > 30)
-				printVInfos(vInfos);
-			else
+			char bFName[200];
+			char bTime[9];
+			sprintf(bFName, "%s %s %s.bat", file->d_name, convertTime(bTime, (int) vInfos->duration), vInfos->codec);
+			char * fileInW = scat(folderInWindows, file->d_name);
+			char * fileOutW = scat(folderOutWindows, vInfos->filename);
+			char * fileBW = scat("***REMOVED***", bFName);
+			char * fileBM = scat("***REMOVED***", bFName);
+			FILE * filee = fopen(fileBM, "w");
+			if(filee != NULL)
 			{
-				char * fileInW = scat(folderInW, file->d_name);
-				char * fileOutW = scat(folderOutW, file->d_name);
-				char * fileW = scat("***REMOVED***", file->d_name);
-				char * fileBW = scat(fileW, ".bat");
-				char * fileM = scat("***REMOVED***", file->d_name);
-				char * fileBM = scat(fileM, ".bat");
-				FILE * filee = fopen(fileBM, "w");
-				fprintf(filee, "mkdir \"%s\"\r\n", folderOutW);
+				fprintf(filee, "mkdir \"%s\"\r\n", folderOutWindows);
 				fprintf(filee, "ffmpeg -n -i \"%s\" -c:v libx265 -preset medium -crf 28 -c:a aac -b:a 128k \"%s\"\r\n", fileInW, fileOutW);
-				fprintf(filee, "if exist \"%s\" del \"%s\"\r\n", fileOutW, fileInW);
+				fprintf(filee, "if exist \"%s\" call \"D:\\Documents\\Logiciels\\deleteJS.bat\" \"%s\"\r\n", fileOutW, fileInW);
 				fprintf(filee, "if exist \"%s\" del \"%s\"\r\n", fileBW, fileBW);
 				fclose(filee);
-				printf("Wrote file %s\n", fileBM);
-				free(fileBM);
-				free(fileM);
-				free(fileBW);
-				free(fileW);
-				free(fileOutW);
-				free(fileInW);
 			}
+			else
+				printf("Error writing file %s\n", fileBM);
+			free(fileBM);
+			free(fileBW);
+			free(fileOutW);
+			free(fileInW);
+			free(vInfos->filename);
+		}
+		else
+		{
+			if(strcmp(vInfos->codec, "hevc") == 0)
+			{
+			}
+			else
+				printf("Skipped file (%s, %lf): %s\n", vInfos->codec, vInfos->fps, vInfos->filename);
 		}
 		free(vInfos);
 	}
@@ -74,7 +87,7 @@ int main()
 	return 0;
 }
 
-char * scat(char * s1, char * s2)
+char * scat(char * s1, const char * s2)
 {
 	char * str = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 	strcpy(str, s1);
@@ -87,17 +100,28 @@ void printVInfos(VInfos * vInfos)
 	printf("File:%s\n\tCodec:\t%s\n\tFPS:\t%lf\n", vInfos->filename, vInfos->codec, vInfos->fps);
 }
 
-VInfos * getVInfos(char * filename)
+char * convertTime(char * out, int time)
+{
+	int sec = time % 60;
+	time /= 60;
+	int min = time % 60;
+	time /= 60;
+	sprintf(out, "%02d.%02d.%02d", time, min, sec);
+	return out;
+}
+
+VInfos * getVInfos(char * filename, const char * name)
 {
 	VInfos * vInfos = (VInfos *) malloc(sizeof(VInfos));
 	vInfos->codec = NULL;
 	vInfos->fps = 0;
-	vInfos->filename = filename;
+	vInfos->duration = 0;
+	vInfos->filename = asMP4(name);
 	
 	AVFormatContext * pFormatCtx = avformat_alloc_context();
 	int errorID = avformat_open_input(&pFormatCtx, filename, NULL, NULL);
 	
-	if(errorID < 0)
+	if(errorID < 0 || pFormatCtx->nb_streams == 0)
 	{
 		char * errorStr;
 		errorStr = (char *) malloc(100 * sizeof(char));
@@ -109,6 +133,7 @@ VInfos * getVInfos(char * filename)
 	}
 	else
 	{
+		vInfos->duration = pFormatCtx->duration / ((double) AV_TIME_BASE);
 		for(unsigned int i = 0; i < pFormatCtx->nb_streams; i++)
 		{
 			AVStream * stream = pFormatCtx->streams[i];
@@ -137,7 +162,21 @@ int shouldProcessFile(char * filename)
 	if(*filename == '.')
 		return 0;
 	char * dot = strrchr(filename, '.');
-	if(dot == NULL || strcmp(dot, ".jpg") == 0)
+	if(dot == NULL || strcmp(dot, ".jpg") == 0 || strcmp(dot, ".png") == 0 || strcmp(dot, ".mjpeg") == 0 || strcmp(dot, ".ini") == 0)
 		return 0;
 	return 1;
+}
+
+char * asMP4(const char * filename)
+{
+	char * nFilename = strdup(filename);
+	char * dot = strrchr(nFilename, '.');
+	if(strcmp(dot, ".mp4") != 0)
+	{
+		if(strlen(dot) < 4)
+			realloc(&nFilename, sizeof(char) * ((dot - nFilename) + 5));
+		dot = strrchr(nFilename, '.');
+		strcpy(dot, ".mp4");
+	}
+	return nFilename;
 }
